635a223793115b673ec74f6fef8fcb4d054563d5
Alexandre Fontoura <xesfnet@gmail.com>
2018-11-11 01:10:22 +0000

    Adding LZW decompression from https://rosettacode.org/wiki/LZW_compression#ES6_Version

--- tools/dump/src/compression/lzw.js
+++ tools/dump/src/compression/lzw.js
@@ -1,4 +1,46 @@
+// from https://rosettacode.org/wiki/LZW_compression#ES6_Version
 
 export function decompressLZW(data) {
-    return data;
+    // Initialize Dictionary (inverse of compress)
+    let dict = {};
+    for (let i = 0; i < 256; i++)
+    {
+        dict[i] = String.fromCharCode(i);
+    }
+
+    let word = String.fromCharCode(data[0]);
+    let result = word;
+    let entry = '';
+    let dictSize = 256;
+
+    for (let i = 1, len = data.length; i < len; i++)
+    {
+        let curNumber = data[i];
+
+        if (dict[curNumber] !== undefined)
+        {
+            entry = dict[curNumber];
+        }
+        else
+        {
+            if (curNumber === dictSize)
+            {
+                entry = word + word[0];
+            }
+            else
+            {
+                throw 'Error in processing';
+                return null;
+            }
+        }
+
+        result += entry;
+
+        // Add word + entry[0] to dictionary
+        dict[dictSize++] = word + entry[0];
+
+        word = entry;
+    }
+
+    return result;
 }
