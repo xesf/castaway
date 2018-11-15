7d7d1e6b3f0835246efb6438079e996ad734b686
Alexandre Fontoura <xesfnet@gmail.com>
2018-11-15 23:59:50 +0000

    Starting to draw the images. Need to do it based on canvas size and image size to get the correct viewport.

--- projects/viewer/src/components/ResourceView.jsx
+++ projects/viewer/src/components/ResourceView.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useRef, useLayoutEffect } from 'react'
 
 const ResourceView = ({ data }) => {
+    const canvasRef = useRef();
+    const image = data.images[0];
+    
+    useLayoutEffect(() => {
+        const context = canvasRef.current.getContext("2d");
+        const img = context.createImageData(640, 480); //image.width, image.height);
+        for (let p = 0; p < image.pixels.length; p++) {
+            img.data[p + 0] = image.pixels[p].r;
+            img.data[p + 1] = image.pixels[p].g;
+            img.data[p + 2] = image.pixels[p].b;
+            img.data[p + 3] = image.pixels[p].a;
+        }
+        context.fillRect(0, 0, 640, 480);
+        context.putImageData(img, 0, 0);
+        return () => {}
+    });
+
     return (
         <div>
-            
+            <canvas ref={canvasRef} width="640" height="480" />
         </div>
     );
 };
