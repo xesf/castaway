0b28905a0bd227cc59ef3cc8af936e2c5f8c6ff9
Alexandre Fontoura <xesfnet@gmail.com>
2018-11-17 15:19:40 +0000

    Fixed drawing image pixels

--- projects/viewer/src/components/ResourceView.jsx
+++ projects/viewer/src/components/ResourceView.jsx
@@ -6,13 +6,14 @@ const ResourceView = ({ data }) => {
     
     useLayoutEffect(() => {
         const context = canvasRef.current.getContext("2d");
-        const img = context.createImageData(640, 480); //image.width, image.height);
+        const img = context.createImageData(image.width, image.height);
         for (let p = 0; p < image.pixels.length; p++) {
-            img.data[p + 0] = image.pixels[p].r;
-            img.data[p + 1] = image.pixels[p].g;
-            img.data[p + 2] = image.pixels[p].b;
-            img.data[p + 3] = image.pixels[p].a;
+            img.data[(p * 4) + 0] = image.pixels[p].r;
+            img.data[(p * 4) + 1] = image.pixels[p].g;
+            img.data[(p * 4) + 2] = image.pixels[p].b;
+            img.data[(p * 4) + 3] = image.pixels[p].a;
         }
+        context.fillStyle = 'white';
         context.fillRect(0, 0, 640, 480);
         context.putImageData(img, 0, 0);
         return () => {}
