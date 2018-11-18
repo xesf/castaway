4ab59f98c6a438ad41d461387e2d1668302a03e3
Alexandre Fontoura <xesfnet@gmail.com>
2018-11-18 19:37:57 +0000

    Adding RLE decompress mode

--- packages/lifeboat/src/compression/rle.js
+++ packages/lifeboat/src/compression/rle.js
@@ -1,4 +1,19 @@
 
 export function decompressRLE(data, offset, length) {
-    throw 'Decompress Type RLE not implemented';
+    const pdata = [];
+    while (offset < length) {
+        const control = data.getUint8(offset++, true);
+        if ((control & 0x80) === 0x80) {
+            const len = (control & 0x7F);
+            const value = data.getUint8(offset++, true);
+            for (let i = 0; i < len; i++) {
+                pdata.push(value);
+            }
+        } else {
+            for (let i = 0; i < control; i++) {
+                pdata.push(data.getUint8(offset++, true));
+            }
+        }
+    }
+    return pdata;
 }
